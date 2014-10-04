9ae981ee168a11006cb06db8a60a1d20cc034b25
deglingo <ahmidmefes@gmail.com>
2014-10-05 00:29:40 +0200

    added the main dialog

--- src/sbdmain.c
+++ src/sbdmain.c
@@ -2,6 +2,7 @@
  */
 
 #include <clog.h>
+#include <altk.h>
 
 
 
@@ -10,6 +11,16 @@
 int main ( int argc,
            char **argv )
 {
+  AltkDisplay *display;
+  AltkWidget *dlg;
+  GMainLoop *loop;
   CL_DEBUG("hello!");
+  altk_init();
+  display = altk_display_new();
+  dlg = altk_dialog_new(display);
+  altk_widget_show_all(dlg);
+  altk_display_open(display);
+  loop = g_main_loop_new(NULL, FALSE);
+  g_main_loop_run(loop);
   return 0;
 }
