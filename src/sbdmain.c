143680b2bce70539793a19448c7ef7006a985c5e
deglingo <ahmidmefes@gmail.com>
2014-10-05 01:39:03 +0200

    added the game timer

--- src/sbdmain.c
+++ src/sbdmain.c
@@ -7,6 +7,36 @@
 
 
 
+#define SIM_FPS ((gdouble) 10.0)
+
+
+
+struct timer_data
+{
+  SbSim *sim;
+  GTimer *timer;
+  guint frame;
+  gdouble next_frame;
+};
+
+
+
+/* _timer:
+ */
+static gboolean _timer ( struct timer_data *data )
+{
+  while (g_timer_elapsed(data->timer, NULL) >= data->next_frame)
+    {
+      sb_sim_update(data->sim);
+      data->frame++;
+      CL_DEBUG("frame: %d", data->frame);
+      data->next_frame = ((gdouble)(data->frame + 1)) / SIM_FPS;
+    }
+  return G_SOURCE_CONTINUE;
+}
+
+
+
 /* main:
  */
 int main ( int argc,
@@ -17,6 +47,8 @@ int main ( int argc,
   GMainLoop *loop;
   SbSim *sim;
   SbBot *bot;
+  GTimer *timer;
+  struct timer_data timer_data;
   CL_DEBUG("hello!");
   altk_init();
   sim = sb_sim_new();
@@ -26,6 +58,16 @@ int main ( int argc,
   altk_widget_show_all(dlg);
   altk_display_open(display);
   loop = g_main_loop_new(NULL, FALSE);
+  timer = g_timer_new();
+  timer_data.sim = sim;
+  timer_data.timer = timer;
+  timer_data.frame = 0;
+  timer_data.next_frame = 1.0 / SIM_FPS;
+  g_timeout_add_full(0 /* [fixme] priority */,
+                     1,
+                     (GSourceFunc) _timer,
+                     &timer_data,
+                     NULL);
   g_main_loop_run(loop);
   return 0;
 }
