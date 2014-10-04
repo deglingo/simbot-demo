c9b817079d4d480dbbbec48ad54bd99a3ff006d5
deglingo <ahmidmefes@gmail.com>
2014-10-05 01:03:53 +0200

    created the SbSim object

--- src/sbdmain.c
+++ src/sbdmain.c
@@ -3,6 +3,7 @@
 
 #include <clog.h>
 #include <altk.h>
+#include <simbot.h>
 
 
 
@@ -14,8 +15,10 @@ int main ( int argc,
   AltkDisplay *display;
   AltkWidget *dlg;
   GMainLoop *loop;
+  SbSim *sim;
   CL_DEBUG("hello!");
   altk_init();
+  sim = sb_sim_new();
   display = altk_display_new();
   dlg = altk_dialog_new(display);
   altk_widget_show_all(dlg);
